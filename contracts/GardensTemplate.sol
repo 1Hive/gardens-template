570b436d7763faf6781bac907723c06a92ca4278
William Griffiths <willjgriff@gmail.com>
2020-04-17 15:29:15 +0100

    Added caching of contracts. Added Tollgate. Updated new dao script.

--- contracts/GardensTemplate.sol
+++ contracts/GardensTemplate.sol
@@ -2,6 +2,7 @@ pragma solidity 0.4.24;
 
 import "@aragon/templates-shared/contracts/BaseTemplate.sol";
 import "@1hive/apps-dandelion-voting/contracts/DandelionVoting.sol";
+import "./external/ITollgate.sol";
 
 contract GardensTemplate is BaseTemplate {
 
@@ -13,6 +14,15 @@ contract GardensTemplate is BaseTemplate {
     uint256 constant private TOKEN_MAX_PER_ACCOUNT = uint256(-1);
     uint64 constant private DEFAULT_FINANCE_PERIOD = uint64(30 days);
 
+    struct DeployedContracts {
+        Kernel dao;
+        ACL acl;
+        DandelionVoting dandelionVoting;
+        Vault agentOrVault;
+    }
+
+    mapping(address => DeployedContracts) internal senderDeployedContracts;
+
     constructor(DAOFactory _daoFactory, ENS _ens, MiniMeTokenFactory _miniMeFactory, IFIFSResolvingRegistrar _aragonID)
         BaseTemplate(_daoFactory, _ens, _miniMeFactory, _aragonID)
         public
@@ -21,10 +31,11 @@ contract GardensTemplate is BaseTemplate {
         _ensureMiniMeFactoryIsValid(_miniMeFactory);
     }
 
-    function createDao(
+    // New DAO functions //
+
+    function createDaoTxOne(
         string _voteTokenName,
         string _voteTokenSymbol,
-        string _id,
         address[] _members,
         uint64[5] _votingSettings,
         uint64 _financePeriod,
@@ -33,7 +44,6 @@ contract GardensTemplate is BaseTemplate {
     )
         public
     {
-        _validateId(_id);
         _ensureMembershipSettings(_members, _votingSettings);
 
         (Kernel dao, ACL acl) = _createDAO();
@@ -57,10 +67,22 @@ contract GardensTemplate is BaseTemplate {
         _createEvmScriptsRegistryPermissions(acl, dandelionVoting, dandelionVoting);
         _createCustomVotingPermissions(acl, dandelionVoting, tokenManager);
         _createCustomTokenManagerPermissions(acl, tokenManager, dandelionVoting);
-        _transferRootPermissionsFromTemplateAndFinalizeDAO(dao, dandelionVoting);
 
+        _storeDeployedContractsTxOne(dao, acl, dandelionVoting, agentOrVault);
+    }
+
+    function createDaoTxTwo(string _id, ERC20 _tollgateFeeToken, uint256 _tollgateFeeAmount) external {
+        _validateId(_id);
+        (Kernel dao, ACL acl, DandelionVoting dandelionVoting, Vault agentOrVault) = _getDeployedContracts();
+
+        ITollgate tollgate = _installTollgate(dao, _tollgateFeeToken, _tollgateFeeAmount, address(agentOrVault));
+        _createTollgatePermissions(acl, tollgate, dandelionVoting);
+
+        _transferRootPermissionsFromTemplateAndFinalizeDAO(dao, dandelionVoting);
         _registerID(_id, dao);
     }
+
+    // App installation functions //
 
     function _installDandelionVotingApp(Kernel _dao, MiniMeToken _voteToken, uint64[5] _votingSettings)
         internal returns (DandelionVoting)
@@ -72,19 +94,60 @@ contract GardensTemplate is BaseTemplate {
         return dandelionVoting;
     }
 
-    function _createCustomVotingPermissions(ACL _acl, DandelionVoting _dandelionVoting, TokenManager _tokenManager) internal {
+    function _installTollgate(Kernel _dao, ERC20 _tollgateFeeToken, uint256 _tollgateFeeAmount, address _tollgateFeeDestination)
+        internal returns (ITollgate)
+    {
+        bytes32 tollgateAppId = apmNamehash("tollgate");
+        ITollgate tollgate = ITollgate(_installNonDefaultApp(_dao, tollgateAppId));
+        tollgate.initialize(_tollgateFeeToken, _tollgateFeeAmount, _tollgateFeeDestination);
+        return tollgate;
+    }
+
+    // Permission setting functions //
+
+    function _createCustomVotingPermissions(ACL _acl, DandelionVoting _dandelionVoting, TokenManager _tokenManager)
+        internal
+    {
         _acl.createPermission(_tokenManager, _dandelionVoting, _dandelionVoting.CREATE_VOTES_ROLE(), _dandelionVoting);
         _acl.createPermission(_dandelionVoting, _dandelionVoting, _dandelionVoting.MODIFY_QUORUM_ROLE(), _dandelionVoting);
         _acl.createPermission(_dandelionVoting, _dandelionVoting, _dandelionVoting.MODIFY_SUPPORT_ROLE(), _dandelionVoting);
     }
 
-    function _createCustomTokenManagerPermissions(ACL _acl, TokenManager _tokenManager, DandelionVoting _dandelionVoting) internal {
+    function _createCustomTokenManagerPermissions(ACL _acl, TokenManager _tokenManager, DandelionVoting _dandelionVoting)
+        internal
+    {
         _acl.createPermission(_dandelionVoting, _tokenManager, _tokenManager.BURN_ROLE(), _dandelionVoting);
         _acl.createPermission(_dandelionVoting, _tokenManager, _tokenManager.MINT_ROLE(), _dandelionVoting);
     }
 
+    function _createTollgatePermissions(ACL _acl, ITollgate _tollgate, DandelionVoting _dandelionVoting) internal {
+        _acl.createPermission(_dandelionVoting, _tollgate, _tollgate.CHANGE_AMOUNT_ROLE(), _dandelionVoting);
+        _acl.createPermission(_dandelionVoting, _tollgate, _tollgate.CHANGE_DESTINATION_ROLE(), _dandelionVoting);
+    }
+
+    // Validation functions //
+
     function _ensureMembershipSettings(address[] memory _members, uint64[5] memory _votingSettings) private pure {
         require(_members.length > 0, ERROR_MISSING_MEMBERS);
         require(_votingSettings.length == 5, ERROR_BAD_VOTE_SETTINGS);
+    }
+
+    // Temporary Storage functions //
+
+    function _storeDeployedContractsTxOne(Kernel _dao, ACL _acl, DandelionVoting _dandelionVoting, Vault _agentOrVault) internal {
+        DeployedContracts storage deployedContracts = senderDeployedContracts[msg.sender];
+        deployedContracts.dao = _dao;
+        deployedContracts.acl = _acl;
+        deployedContracts.dandelionVoting = _dandelionVoting;
+        deployedContracts.agentOrVault = _agentOrVault;
+    }
+
+    function _getDeployedContracts() internal returns (Kernel, ACL, DandelionVoting, Vault) {
+        DeployedContracts storage deployedContracts = senderDeployedContracts[msg.sender];
+        return (deployedContracts.dao, deployedContracts.acl, deployedContracts.dandelionVoting, deployedContracts.agentOrVault);
+    }
+
+    function _deleteStoredContracts() internal {
+        delete senderDeployedContracts[msg.sender];
     }
 }
--- scripts/new-dao.js
+++ scripts/new-dao.js
@@ -1,4 +1,5 @@
 const GardensTemplate = artifacts.require("GardensTemplate")
+const Token = artifacts.require("Token")
 
 const DAO_ID = "05" // Note this must be unique for each deployment, change it for subsequent deployments
 const INITIAL_SUPERVISOR = "0xb4124cEB3451635DAcedd11767f004d8a28c6eE7"
@@ -30,22 +31,34 @@ const VOTE_BUFFER_BLOCKS = 10
 const VOTE_EXECUTION_DELAY_BLOCKS = 5
 const VOTING_SETTINGS = [SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM, VOTE_DURATION_BLOCKS, VOTE_BUFFER_BLOCKS, VOTE_EXECUTION_DELAY_BLOCKS]
 
+const TOLLGATE_FEE = 1e18 // 1 DAI
+
 module.exports = async (callback) => {
   try {
     const gardensTemplate = await GardensTemplate.at(gardensTemplateAddress())
 
-    const createDaoReceipt = await gardensTemplate.createDao(
+    const tollgateToken = await Token.new(INITIAL_SUPERVISOR, "DAI", "DAI")
+    console.log(`Created DAI Token: ${tollgateToken.address}`)
+
+    const createDaoTxOneReceipt = await gardensTemplate.createDaoTxOne(
       "VoteToken",
       "VTT",
-      daoId(),
       [INITIAL_SUPERVISOR],
       VOTING_SETTINGS,
       0,
       true,
       false
     );
-    console.log(`DEBUG: ${createDaoReceipt.logs.find(x => x.event === "DEBUG")} `)
-    console.log(`DAO address: ${createDaoReceipt.logs.find(x => x.event === "DeployDao").args.dao} Gas used: ${createDaoReceipt.receipt.gasUsed} `)
+    // console.log(`DEBUG: ${createDaoTxOneReceipt.logs.find(x => x.event === "DEBUG")} `)
+    console.log(`Tx One Complete. DAO address: ${createDaoTxOneReceipt.logs.find(x => x.event === "DeployDao").args.dao} Gas used: ${createDaoTxOneReceipt.receipt.gasUsed} `)
+
+    const createDaoTxTwoReceipt = await gardensTemplate.createDaoTxTwo(
+      daoId(),
+      tollgateToken.address,
+      TOLLGATE_FEE
+    )
+    console.log(`Tx Two Complete. Gas used: ${createDaoTxTwoReceipt.receipt.gasUsed} `)
+
 
   } catch (error) {
     console.log(error)
