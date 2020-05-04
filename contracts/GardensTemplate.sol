d415eb09145a06752a6809edd0d96b45d57f97e1
William Griffiths <willjgriff@gmail.com>
2020-05-04 17:57:38 +0100

    Expand error strings. Rearrange app id.

--- contracts/GardensTemplate.sol
+++ contracts/GardensTemplate.sol
@@ -15,9 +15,9 @@ import {IAragonFundraisingController as Controller} from "./external/IAragonFund
 
 contract GardensTemplate is BaseTemplate {
 
-    string constant private ERROR_MISSING_MEMBERS = "WRONG_MEMBS";
-    string constant private ERROR_BAD_VOTE_SETTINGS = "BAD_SETT";
-    string constant private ERROR_MISSING_CACHE = "NO_CACHE";
+    string constant private ERROR_MISSING_MEMBERS = "MISSING_MEMBERS";
+    string constant private ERROR_BAD_VOTE_SETTINGS = "BAD_SETTINGS";
+    string constant private ERROR_NO_CACHE = "NO_CACHE";
     string constant private ERROR_NO_COLLATERAL = "NO_COLLATERAL";
     string constant private ERROR_NO_TOLLGATE_TOKEN = "NO_TOLLGATE_TOKEN";
 
@@ -37,13 +37,13 @@ contract GardensTemplate is BaseTemplate {
     bytes32 private constant REDEMPTIONS_APP_ID = 0xbf9fefd9508fe20f068aa3714e9beb8c4fc6dea33dc4c75371b96140d6350d20; // Local
     bytes32 private constant CONVICTION_VOTING_APP_ID = 0x589851b3734f6578a92f33bfc26877a1166b95238be1f484deeaac6383d14c38; // Local
     bytes32 private constant TOLLGATE_APP_ID = 0x7075e547e73484f0736b2160fcfb010b4f32b751fc729c25b677a0347d9b4246; // Local
-    bytes32 private constant HOOKED_TOKEN_MANAGER_APP_ID = 0x28b7f7bcb4bfd2310258c158a57a1307f45a70858cde9298734a6db37c6ca492; // Local
 
 //    bytes32 private constant DANDELION_VOTING_APP_ID = 0x2d7442e1c4cb7a7013aecc419f938bdfa55ad32d90002fb92ee5969e27b2bf07; // Rinkeby
 //    bytes32 private constant REDEMPTIONS_APP_ID = 0x743bd419d5c9061290b181b19e114f36e9cc9ddb42b4e54fc811edb22eb85e9d; // Rinkeby
 //    bytes32 private constant CONVICTION_VOTING_APP_ID = 0x16c0b0af27b5e169e5f678055840d7ab2b312519d7700a06554c287619f4b9f9; // Rinkeby
 //    bytes32 private constant TOLLGATE_APP_ID = 0x0d321283289e70165ef6db7f11fc62c74a7d39dac3ee148428c4f9e3d74c6d61; // Rinkeby
 
+    bytes32 private constant HOOKED_TOKEN_MANAGER_APP_ID = 0x28b7f7bcb4bfd2310258c158a57a1307f45a70858cde9298734a6db37c6ca492;
     bytes32 private constant BANCOR_FORMULA_ID = 0xd71dde5e4bea1928026c1779bde7ed27bd7ef3d0ce9802e4117631eb6fa4ed7d;
     bytes32 private constant PRESALE_ID = 0x5de9bbdeaf6584c220c7b7f1922383bcd8bbcd4b48832080afd9d5ebf9a04df5;
     bytes32 private constant MARKET_MAKER_ID= 0xc2bb88ab974c474221f15f691ed9da38be2f5d37364180cec05403c656981bf0;
@@ -137,7 +137,7 @@ contract GardensTemplate is BaseTemplate {
     {
         require(_tollgateFeeToken != address(0), ERROR_NO_TOLLGATE_TOKEN);
         require(_collateralToken != address(0), ERROR_NO_COLLATERAL);
-        require(senderDeployedContracts[msg.sender].dao != address(0), ERROR_MISSING_CACHE);
+        require(senderDeployedContracts[msg.sender].dao != address(0), ERROR_NO_CACHE);
 
         (,
         ACL acl,
@@ -201,7 +201,7 @@ contract GardensTemplate is BaseTemplate {
     )
         public
     {
-        require(senderDeployedContracts[msg.sender].collateralToken != address(0), ERROR_MISSING_CACHE);
+        require(senderDeployedContracts[msg.sender].collateralToken != address(0), ERROR_NO_CACHE);
 
         _installFundraisingApps(
             _goal,
@@ -244,7 +244,7 @@ contract GardensTemplate is BaseTemplate {
     )
         public
     {
-        require(senderDeployedContracts[msg.sender].reserveVault != address(0), ERROR_MISSING_CACHE);
+        require(senderDeployedContracts[msg.sender].reserveVault != address(0), ERROR_NO_CACHE);
 
         _validateId(_id);
         (Kernel dao, ACL acl, DandelionVoting dandelionVoting,,) = _getDeployedContractsTxOne();
